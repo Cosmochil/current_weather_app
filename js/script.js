15155607edc05bed51e4c5206c1a37fa12717b13
Cosmochil <tochiruchiru@gmail.com>
2021-02-14 09:15:01 +0700

    weather app

--- js/script.js
+++ js/script.js
@@ -6,40 +6,42 @@ let display = document.querySelector(`#display`);
 let longitude;
 let latitude;
 
-function f_to_c(temp) {
-  return (temp - 32) / 1.8
-  
-}
 async function weather_forecast(lat, lon) {
   const response = await fetch(
-    `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3c074934c56a463e427c8b09c10b8493`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=3c074934c56a463e427c8b09c10b8493`
   );
   const data = await response.json();
   display.innerHTML = `
-    <section>
-      <h5>Location</h5>
-      <h3 class="data">Country: ${data.sys.country}</h3>
-      <h3 class="data">City: ${data.name}</h3>
-    </section>
-    <section>
-      <h5>Current<br>Weather</h5>
-      <h3 class="data">${data.weather[0].main}</h3>
-      <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
-    </section>
-    <section>
-      <h5>Current<br>Temperature</h5>
-      <img class="heat" src="./images/heat.svg">
-      <h3 class="data">${f_to_c(data.main.temp).toFixed(2)}</h3>
-    </section>
-    <section>
-      <h5>Wind Speed</h5>
-      <img class="wind" src="./images/anemometer.svg">
-      <h3 class="data">${data.wind.speed} miles/hr</h3>
-    </section>
-    <section>
-      <h5>Humidity</h5>
-      <h3 class="data">${data.main.humidity}</h3>
-    </section>
+    <div class="location">
+      <section>
+        <h3 class="data">Country: ${data.sys.country}</h3>
+      </section>
+      <section>
+        <h3 class="data">City: ${data.name}</h3>
+      </section>
+    </div>
+    <div class="forecast">
+      <section>
+        <h5>Current<br>Weather</h5>
+        <img class="weather" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+        <h3 class="data">${data.weather[0].main}</h3>
+      </section>
+      <section>
+        <h5>Temperature</h5>
+        <img class="heat" src="./images/heat.svg">
+        <h3 class="data">${(data.main.temp).toFixed(2)}°C</h3>
+      </section>
+      <section>
+        <h5>Wind Speed</h5>
+        <img class="wind" src="./images/anemometer.svg">
+        <h3 class="data">${data.wind.speed} mtr/sec</h3>
+      </section>
+      <section>
+        <h5>Humidity</h5>
+        <img class="humidity" src="./images/humidity.svg">
+        <h3 class="data">${data.main.humidity}%</h3>
+      </section>
+    </div>
     `;
 }
 
